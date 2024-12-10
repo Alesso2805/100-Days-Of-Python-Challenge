d1a867af367c4b31b8556a8c7bbaba81e5a76215
Alessandro David <alessandrodp2222@gmail.com>
2024-12-10 08:24:52 -0500

    day 2 almost completed

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,18 @@
-len("Hello")
+#subscripting
+print("Hello"[0])
+
+#String
+print("My name is" + " Alesso")
+
+#Integer - Whole number
+print(12421 + 21512412)
+
+#Large Integers
+print(2443_12312_12543)
+
+#Float = Floating Point Number
+print(3.1411249)
+
+#Boolean
+print(True)
+print(False)
