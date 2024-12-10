a2fc54b3022a2c0d28beb1a6e8154ebd277cc2ed
Alessandro Alonso David Polanco <u202122232@upc.edu.pe>
2024-12-10 15:26:14 -0500

    day 2 fully completed

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,12 @@
 bmi = 84 / 1.65 ** 2
+
+print(round(bmi, 2))
+
+score = 0
+
+#User scores a point
+score += 1
+print(score)
+
+# f - strings
+print(f"Alessandro has scored {score} goals ")
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,8 @@
-len(12345)
+print(len("Hello my name is Alessandro"))
+
+print(type("abcdefg"))
+print(type(123))
+print(type(31.45))
+print(type(True))
+
+print("Number of letters in your name: " + str(len(input("Enter your name:"))))
