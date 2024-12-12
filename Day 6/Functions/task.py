ac88d1c93a9733427850f69f57c51b3657a79ead
Alessandro Alonso David Polanco <u202122232@upc.edu.pe>
2024-12-12 15:02:45 -0500

    day 6 - functions: almost completed

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,9 @@
+sentence = input("Write something and we'll count the number of letter in it: ")
+letters = (len(sentence))
+print(f"Your sentence has {letters} letters on it")
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
