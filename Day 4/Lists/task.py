2e279b1a90e63b90ad3cb51021d0bd3c487d2024
Alessandro David <alessandrodp2222@gmail.com>
2024-12-12 09:13:04 -0500

    day 5 completed

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,7 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[12])
+
+states_of_america.extend(["AlessoLand", "Maryland"])
+
+print(states_of_america)
